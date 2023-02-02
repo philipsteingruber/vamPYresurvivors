9dded352f46439aa33f9e095777a60d041e647b2
= <=>
2023-02-02 19:38:22 +0100

    Initial commit

---
+++ level.py
@@ -0,0 +1,9 @@
+import pygame
+
+
+class Level:
+    def __init__(self):
+        pass
+
+    def run(self, dt):
+        pass
---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import sys
+
+import pygame
+
+from settings import DIMENSIONS
+from level import Level
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(tuple(DIMENSIONS.values()))
+        pygame.display.set_caption('vamPYre survivors')
+        self.clock = pygame.time.Clock()
+        self.level = Level()
+
+    def run(self) -> None:
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit(0)
+
+            dt = self.clock.tick() / 1000
+            self.level.run(dt)
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    lines = 0
+    for filename in os.listdir('./'):
+        try:
+            with open(filename) as file:
+                linecount = len(file.readlines())
+                print(filename, linecount)
+                lines += linecount
+        except PermissionError:
+            pass
+    print('Full Linecount:', lines)
+    game = Game()
+    game.run()
