12a8d4360157936073f756f3371966951f86ac4f
= <=>
2023-02-23 15:12:40 +0100

    Added assets

---
+++ assets/map/tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileset1" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="C:/Users/phili/Downloads/hyptosis_tile-art-batch-1.png" width="960" height="960"/>
+</tileset>
